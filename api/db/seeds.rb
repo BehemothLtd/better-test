aa22ee4521b82c5595a327033d38aa0239c56f9b
vuth <vuth@behemoth.vn>
2022-05-28 12:46:49 +0700

    update seed

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Project.create([{ name: "Chaty" }, { name: "Liverl" }])
+screen = Project.first.screens.build
+screen.name = "React"
+screen.url = "/console/reacts"
+screen.save
+
+tc = screen.test_cases.build
+tc.url = "/console/reacts"
+tc.name = "test react"
+tc.steps = [
+  {
+    "command": "open",
+    "selector_type": "url",
+    "selector_path": "/console/reacts/",
+    "value": ""
+  },
+  {
+    "command": "click",
+    "selector_type": "css",
+    "selector_path": "tr:nth-child(1) .mdi-pencil",
+    "value": ""
+  },
+  {
+    "command": "click",
+    "selector_type": "css",
+    "selector_path": ".mdi-chat-outline",
+    "value": ""
+  },
+  {
+    "command": "click",
+    "selector_type": "css",
+    "selector_path": "#reactTextCollapse > .form-control",
+    "value": ""
+  },
+  {
+    "command": "type",
+    "selector_type": "css",
+    "selector_path": "#reactTextCollapse > .form-control",
+    "value": "qqqqqqq"
+  },
+  {
+    "command": "click",
+    "selector_type": "css",
+    "selector_path": ".btn-primary",
+    "value": ""
+  },
+  {
+    "command": "waitForText",
+    "selector_type": "css",
+    "selector_path": ".toasted-container.top-right",
+    "value": "キーワード応答を更新しました"
+  }
+]
