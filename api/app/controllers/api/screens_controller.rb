b10b90a1101a361b3a99b98e5079d98402fbd473
behemoth-cuongdv <cuongdv@behemoth.vn>
2022-05-28 17:37:20 +0700

    refactor

--- api/app/controllers/api/screens_controller.rb
+++ api/app/controllers/api/screens_controller.rb
@@ -5,23 +5,18 @@ module Api
       screen = Screen.find(params[:id])
       elements = screen.elements
       render json: screen.serializable_hash.merge(elements: elements)
-
     end
 
     def create
-      begin
         screen = Screen.create(screen_params)
         render json: screen
-      end
     end
 
     def update
       screen = Screen.find(params[:id])
-      begin
-        screen.update(screen_params)
-        render message "Updated"
+      screen.update(screen_params)
+      render json: screen
       end
-    end
 
     def destroy
       screen = Screen.find(params[:id])
