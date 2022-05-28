4080f17d21758ad2910bad8aa6b17da76c3b6d75
vuth <vuth@behemoth.vn>
2022-05-28 12:35:41 +0700

    update relations

--- api/app/models/screen.rb
+++ api/app/models/screen.rb
@@ -1,5 +1,5 @@
 class Screen < ApplicationRecord
-  has_many :elements
-  has_many :test_cases
+  has_many :elements, dependent: :destroy
+  has_many :test_cases, dependent: :destroy
   belongs_to :project
 end
