70fce5189f807bd5daba96f276cefbb34ab87a45
vuth <vuth@behemoth.vn>
2022-05-28 12:35:29 +0700

    wip

--- api/app/controllers/api/elements_controller.rb
+++ api/app/controllers/api/elements_controller.rb
@@ -1,10 +1,32 @@
 module Api
   class ElementsController < ApplicationController
+    def create
+      element = Element.create!(element_params)
+      render json: element
+    end
+
+    def update
+      element = Element.find(params[:id])
+      element.update!(element_params)
+      render json: element
+    end
+
+    def destroy
+      element = Element.find(params[:id])
+      element.destroy!(element_params)
+      render json: element
+    end
+
     def capture_image
-      service = ::CaptureImageService.new(params[:url], params[:auth_id], params[:selector_type], params[:selector_path])
+      service = ::CaptureImageService.new(params[:url], params[:auth_id], params[:selector_type],
+                                          params[:selector_path])
       service.execute!
 
       render json: service.result
+    end
+
+    def element_params
+      params.permit(:name, :selector_type, :selector_path, :image, :screen_id)
     end
   end
 end
