98ccdf718bb543332f186ad4360e8a08c68159ea
vuth <vuth@behemoth.vn>
2022-05-29 12:41:17 +0700

    show test case error msg

---
+++ client/components/common/TestCaseResultDetail.vue
@@ -0,0 +1,41 @@
+<template>
+  <tbody>
+    <tr>
+      <td>
+        <nuxt-link
+          :to="`/projects/${record.project_id}/screens/${record.screen_id}/test_cases/${record.test_case_id}`"
+        >
+          {{ record.test_case_name }}
+        </nuxt-link>
+      </td>
+      <td>{{ record.result }}</td>
+      <td>{{ record.created_at }}</td>
+      <td>
+        <span role="button" class="text-primary" @click="show = !show">
+          Detail
+        </span>
+      </td>
+    </tr>
+    <tr v-show="show">
+      <td colspan="4">
+        <div v-if="record.message" class="text-danger">
+          {{ record.message }}
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</template>
+
+<script>
+export default {
+  props: {
+    record: Object,
+  },
+
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
